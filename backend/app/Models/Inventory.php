02fd0b0efdefb7207b92950591d124c97ff08951
Wildan Taufiq Abdul Aziz <wildantaufiq.abdulaziz@gmail.com>
2022-08-28 21:23:21 +0700

    add lumen generator

--- backend/app/Models/Inventory.php
+++ backend/app/Models/Inventory.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    use HasFactory;
-
     protected $table = 'inventories';
-
+    protected $fillable = [
+        'name', 'detail', 'price', 'status'
+    ];
 }
--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -94,6 +94,8 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+
 
 /*
 |--------------------------------------------------------------------------
